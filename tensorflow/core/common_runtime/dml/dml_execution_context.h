a9dd9d670235a6fee8cc258972a9c24345b862df
Justin Stoecker <justoeck@microsoft.com>
2021-03-09 23:25:38 -0800

    refactor

--- tensorflow/core/common_runtime/dml/dml_execution_context.h
+++ tensorflow/core/common_runtime/dml/dml_execution_context.h
@@ -31,9 +31,14 @@ namespace tensorflow {
 class DmlAllocator;
 class DmlCommandQueue;
 
-// A thread-safe wrapper over DmlExecutionContextImpl. Calls to this class are
-// batched to minimize work within the lock, and the batched calls are
-// periodically flushed by a background thread (or by explicitly calling Flush).
+// A thread-safe object for recording and executing GPU commands. Calls to
+// this class are batched to minimize lock contention, and the batched
+// commands are periodically flushed by a background thread (or by explicitly
+// calling Flush). Unless otherwise stated, it is the caller's responsibility to
+// keep GPU resource arguments (buffers, heaps, DML ops, etc.) alive until they
+// have finished executing on the GPU; the returned GPU event, when signaled,
+// indicates when the submitted work has finished and the associated objects are
+// safe to release.
 class DmlExecutionContext {
  public:
   DmlExecutionContext(ID3D12Device* d3d12_device, IDMLDevice* dml_device,
@@ -41,34 +46,40 @@ class DmlExecutionContext {
 
   ~DmlExecutionContext();
 
-  // NOTE: the caller is responsible for keeping the resources alive until the
-  // returned GPU event has completed.
+  // NOTE: the caller is responsible for keeping the dst_buffer/src_buffer
+  // resources alive until the returned GPU event has completed.
   DmlGpuEvent CopyBufferRegion(ID3D12Resource* dst_buffer, uint64_t dst_offset,
                                D3D12_RESOURCE_STATES dst_state,
                                ID3D12Resource* src_buffer, uint64_t src_offset,
                                D3D12_RESOURCE_STATES src_state,
                                uint64_t byte_count);
 
-  // NOTE: the caller is responsible for keeping the resources alive until the
-  // returned GPU event has completed.
+  // NOTE: the caller is responsible for keeping the dst resource alive until
+  // the returned GPU event has completed. A copy of the value span will be
+  // made, so the pointed-to value is safe to release immediately after calling
+  // this method.
   DmlGpuEvent FillBufferWithPattern(ID3D12Resource* dst, uint64_t dst_offset,
                                     uint64_t dst_size_in_bytes,
                                     absl::Span<const uint8_t> value);
 
   // NOTE: the caller is responsible for keeping the initializer and
-  // descriptor_heap alive until the returned GPU event has completed.
+  // descriptor_heap alive until the returned GPU event has completed. This
+  // class takes ownership of the binding table.
   DmlGpuEvent InitializeOperator(
       IDMLOperatorInitializer* initializer,
       Microsoft::WRL::ComPtr<IDMLBindingTable>&& binding_table,
       ID3D12DescriptorHeap* descriptor_heap);
 
   // NOTE: the caller is responsible for keeping the op and descriptor_heap
-  // alive until the returned GPU event has completed.
+  // alive until the returned GPU event has completed. This class takes
+  // ownership of the binding table.
   DmlGpuEvent ExecuteOperator(
       IDMLCompiledOperator* op,
       Microsoft::WRL::ComPtr<IDMLBindingTable>&& binding_table,
       ID3D12DescriptorHeap* descriptor_heap);
 
+  // NOTE: A copy of the barriers span will be made, so the pointed-to value is
+  // safe to release immediately after calling this method.
   DmlGpuEvent ResourceBarrier(
       absl::Span<const D3D12_RESOURCE_BARRIER> barriers);
 
@@ -76,6 +87,9 @@ class DmlExecutionContext {
   // only includes a UAV barrier (elides an extra copy).
   DmlGpuEvent UavBarrier();
 
+  // Indicates that any batched commands should be recorded and executed as soon
+  // as possible, even if the batch is small. This is a no-op if nothing is
+  // batched.
   StatusOr<DmlGpuEvent> Flush();
 
   Status GetCommandRecorderStatus() const;
@@ -87,21 +101,20 @@ class DmlExecutionContext {
  private:
   static constexpr uint32_t default_batch_flush_size = 100;
   static constexpr uint32_t default_batch_flush_time_us = 1000;
-  static constexpr uint32_t default_num_exec_threads = 1;
 
   using Command = std::function<void(DmlCommandList&)>;
   using Batch = absl::InlinedVector<Command, default_batch_flush_size>;
 
   // State related to the batching of commands, which may be accessed by
-  // both external threads (e.g. DML kernel) and the main internal execution
+  // both external threads (e.g. DML kernels) and the internal execution
   // thread.
   struct BatchState {
     std::mutex mutex;
     DmlGpuEvent next_flush_event;
-    std::condition_variable new_function_enqueued;
+    std::condition_variable command_added;
 
     // Commands are double buffered: callers extend the "write batch" while the
-    // background thread flushes the "execute batch".
+    // execution thread records and executes the "execute batch".
     Batch batches[2];
     uint32_t write_batch_index = 0;
     Batch& WriteBatch() { return batches[write_batch_index]; }
@@ -112,39 +125,16 @@ class DmlExecutionContext {
     Status status;
   };
 
-  // State related to the execution of a batch of commands, which may only be
-  // accessed by internal execution threads.
-  struct ExecutionState {
-    std::mutex mutex;
-    std::condition_variable commands_added;
-    absl::Span<Command> commands;
-    absl::InlinedVector<uint32_t, default_num_exec_threads> command_starts;
-    absl::InlinedVector<uint32_t, default_num_exec_threads> command_counts;
-    absl::InlinedVector<DmlCommandList, default_num_exec_threads> command_lists;
-    absl::InlinedVector<Status, default_num_exec_threads> command_status;
-    DmlGpuEvent batch_completion_event;
-  };
-
   std::shared_ptr<BatchState> batch_state_;
-  std::shared_ptr<ExecutionState> exec_state_;
   std::shared_ptr<DmlCommandQueue> dml_command_queue_;
-  absl::InlinedVector<std::thread, default_num_exec_threads> exec_threads_;
+  std::shared_ptr<DmlCommandList> dml_command_list_;
+  std::thread execution_thread_;
 
-  // Function invoked by the main execution thread. This involves swapping the
-  // command batches, initiating command recording, and executing recorded
-  // command lists.
-  static void MainExecutionThreadProc(
-      std::shared_ptr<BatchState> state,
-      std::shared_ptr<ExecutionState> exec_state,
-      DmlCommandQueue* dml_command_queue, uint32_t batch_flush_size,
-      uint32_t batch_flush_time_us);
-
-  // Function invoked by any additional execution threads. This involves
-  // recording a subset of batched commands and waiting until needed again.
-  static void SecondaryExecutionThreadProc(
-      uint32_t thread_id, std::shared_ptr<ExecutionState> exec_state);
-
-  static void RecordCommands(uint32_t thread_id, ExecutionState* exec_state);
+  static void ExecutionThreadProc(std::shared_ptr<BatchState> batch_state,
+                                  std::shared_ptr<DmlCommandList> command_list,
+                                  std::shared_ptr<DmlCommandQueue> command_queue,
+                                  uint32_t batch_flush_size,
+                                  uint32_t batch_flush_time_us);
 };
 
 }  // namespace tensorflow
